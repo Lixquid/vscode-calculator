9909e311c6f0d5ceacc5d0007b663dbc5c48bff4
Lixquid <lixquid@lixquid.com>
2025-06-03 19:49:19 +0100

    Copy over existing extension code from v0.1.1

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,134 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as copyPaste from "copy-paste";
+import * as math from "mathjs";
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "calculator" is now active!');
+// Variables ///////////////////////////////////////////////////////////////////
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand(
-		"calculator.helloWorld",
-		() => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			vscode.window.showInformationMessage(
-				"Hello World from Calculator!",
-			);
-		},
-	);
+const config = vscode.workspace.getConfiguration("calculator");
 
-	context.subscriptions.push(disposable);
+let widget: vscode.StatusBarItem;
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+function iterateSelections(
+	all: boolean,
+	callback: (input: string) => string,
+): void {
+	const editor = vscode.window.activeTextEditor;
+	const document = editor.document;
+	const selections = editor.selections;
+
+	vscode.window.activeTextEditor.edit((edit) => {
+		for (const selection of selections) {
+			if (selection.isEmpty && !all) continue;
+
+			const text = document.getText(selection);
+
+			try {
+				const result = callback(text);
+				if (result == null) continue;
+				edit.replace(selection, result);
+			} catch (ex) {
+				console.error(ex);
+			}
+		}
+	});
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+function evaluateSelections(): void {
+	iterateSelections(false, (input) => {
+		return input + " = " + math.eval(input).toString();
+	});
+}
+
+function replaceSelections(): void {
+	iterateSelections(false, (input) => {
+		return math.eval(input).toString();
+	});
+}
+
+function countSelections(): void {
+	let count = config.get("count_start", 0);
+	iterateSelections(true, (input) => {
+		count++;
+		return (count - 1).toString();
+	});
+}
+
+function showInputPanel(): void {
+	// This is a bit of a horrible hack.
+	// As a result, there is no way to "cancel" the dialogue box without
+	// entering invalid input.
+	let output;
+
+	vscode.window
+		.showInputBox({
+			prompt: "Enter a Math Expression to evaluate it. Pressing Enter will set the clipboard text to the return value.",
+			placeHolder: "Expression",
+			validateInput: function (expression: string): string {
+				try {
+					output = math.eval(expression);
+					return output.toString();
+				} catch (ex) {
+					output = undefined;
+					return "Error";
+				}
+			},
+		})
+		.then(function (expression: string): void {
+			if (
+				expression == null &&
+				config.get("_debug_disableinputclipboard", false)
+			) {
+				return;
+			}
+
+			if (output == null) return;
+
+			copyPaste.copy(output);
+		});
+}
+
+function onSelection(): void {
+	const editor = vscode.window.activeTextEditor;
+
+	if (editor.selections.length != 1 || editor.selection.isEmpty) return;
+
+	try {
+		widget.text =
+			"= " +
+			math.eval(editor.document.getText(editor.selection)).toString();
+		widget.show();
+	} catch (ex) {}
+}
+
+// Exports /////////////////////////////////////////////////////////////////////
+
+export function activate(context: vscode.ExtensionContext) {
+	// Commands //
+
+	const command = vscode.commands.registerCommand;
+
+	context.subscriptions.push(
+		command("calculator.evaluate", evaluateSelections),
+		command("calculator.replace", replaceSelections),
+		command("calculator.count", countSelections),
+		command("calculator.showInput", showInputPanel),
+	);
+
+	// Widget //
+
+	if (config.get("disable_widget", false)) return;
+
+	widget = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
+	widget.command = "calculator._hide_widget";
+
+	context.subscriptions.push(
+		// Widget object
+		widget,
+		// Internal command to hide the widget when clicked
+		command("calculator._hide_widget", () => widget.hide()),
+		// Subscription to a changing selection to update the widget
+		vscode.window.onDidChangeTextEditorSelection(onSelection),
+	);
+}
